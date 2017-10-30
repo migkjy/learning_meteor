2cf19013453b9b45854035147caf28adbc27b672
migkjy <migkjy@naver.com>
2017-10-30 15:34:38 +0900

    eslint / babel install

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,18 @@
 module.exports = {
-    "extends": "airbnb",
+    "extends": "airbnb-base",
     "plugins": [
         "react",
         "jsx-a11y",
         "import"
-    ]
+    ],
+    rules:{
+        "linebreak-style": [2, "windows"],
+        "no-console": 0,
+    },
+    env: {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "jquery": true
+      },
 };
